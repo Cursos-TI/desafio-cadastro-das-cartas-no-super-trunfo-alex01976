c57f467a93726bb829fad9fd1577a551759214eb
alex01976 <alexgcmnit@gmail.com>
2025-03-18 19:59:48 +0000

    Informando variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,9 +2,22 @@
 
 
 int main() {
+    char Estado [50];
+    char Códigodacarta[50];
+    char Nomedacidade[50];
+    int população;
+    float ÁreaemKm;
+    float PIB;
+    int PontosTurísticos;
+
+
    printf("Desafio Super trunfo - Novato!\n\n");
     
-   printf("Estado A\n");
+   printf("Estado A\n\n");
+
+
+
+
 
 
 
