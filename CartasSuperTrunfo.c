29abfb696e0371bf36d24584c6ae08bd97a8a4f0
alex01976 <alexgcmnit@gmail.com>
2025-03-18 20:09:19 +0000

    Corrigindo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,17 +3,19 @@
 
 int main() {
     char Estado [50];
-    char Códigodacarta[50];
-    char Nomedacidade[50];
-    int população;
-    float ÁreaemKm;
+    char Codigo_da_carta[50];
+    char Nome_da_cidade[50];
+    int populacao;
+    float Area_em_Km;
     float PIB;
-    int PontosTurísticos;
-
+    int Numero_de_Pontos_Turisticos;
 
    printf("Desafio Super trunfo - Novato!\n\n");
     
    printf("Estado A\n\n");
+
+
+
 
 
 
