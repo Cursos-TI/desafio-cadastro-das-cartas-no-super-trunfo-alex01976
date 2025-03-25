d0d57a0cde199d1b391fea4334e2f7815a3ab6b1
alex01976 <alexgcmnit@gmail.com>
2025-03-25 20:19:07 +0000

    usando printf e scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -35,7 +35,30 @@ int main() {
    printf("Número de Pontos Turísticos: \n");
    scanf("%d", &Numero_de_Pontos_Turisticos);
 
+   printf("Estado B\n\n");
+
+   printf("Digite o Estado da carta B: \n");
+   scanf("%s", &Estado);
+
+   printf("Digite o Códidigo da carta: \n");
+   scanf("%S", &Codigo_da_carta);
+
+   printf("Digite o nome da cidade: \n");
+   scanf("%s", &Nome_da_cidade);
+
+   printf("População: \n");
+   scanf("%d", &populacao);
     
+   printf("Área em Km:" \n);
+   scanf("%f", &Area_em_Km);
+   
+   printf("PIB: \n");
+   scanf("%f",&PIB);
+
+   printf("Número de Pontos Turísticos: \n");
+   scanf("%d", &Numero_de_Pontos_Turisticos);
+
+
    printf("Estado da Carta: %s \n" , Estado);
    printf("Código da Carta: %s \n" , Codigo_da_carta);
    printf("A Cidade é: %s \n" , Nome_da_cidade );
